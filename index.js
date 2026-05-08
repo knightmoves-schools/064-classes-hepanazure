7749e6fc0b866509ba5c70435ad8babaf6d64e8b
hepanazure <hepan.azure@students.msswarriors.org>
2026-05-08 18:55:37 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const guests = [
+  new Person('Alice', 'Anderson'),
+  new Person('Bob', 'Brown'),
+  new Person('Cara', 'Carter'),
+];
 
